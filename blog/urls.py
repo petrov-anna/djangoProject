78b81d943ecbddfa7cd08e546172e5ecfe754f3d
petrov-anna <anna.petrov-petrov@yandex.ru>
2023-12-03 23:17:02 +0300

    Add registration and authorization

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from . import views
 
 from .views import BlogListView, AboutPageView, ImputationPageView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
-    path('imputation/', ImputationPageView.as_view(), name='imputation')
+    path('imputation/', ImputationPageView.as_view(), name='imputation'),
+    path('register/', views.register_request, name="register"),
+    path('login/', views.login_request, name="login")
 ]
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+app_name = "blog"
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include(('blog.urls', 'blog'), namespace='blog'))
 ]
